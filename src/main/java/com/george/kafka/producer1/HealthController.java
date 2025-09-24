ab9afc64cdea29db2d1fc9fd8b3d1c7279995428
whho <whho00@gmail.com>
2025-09-25 00:08:17 +0100

    mm9

--- src/main/java/com/george/kafka/producer1/HealthController.java
+++ src/main/java/com/george/kafka/producer1/HealthController.java
@@ -1,10 +1,16 @@
 package com.george.kafka.producer1;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/health")
 public class HealthController {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public HealthController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @GetMapping
     public String healthCheck() {
