31a298a3a52c3222e4d8134283511e60ebbd6eef
whho <whho00@gmail.com>
2025-09-28 15:23:18 +0100

    a2

---
+++ src/test/java/com/george/kafka/producer1/OrderControllerTest.java
@@ -0,0 +1,15 @@
+package com.george.kafka.producer1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+
+    @Test
+    void getOrder() {
+        assertEquals(1,2);
+
+    }
+}
